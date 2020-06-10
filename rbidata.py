02ce037bd533bf27c32bf21d8ca17cf9c5b3831b
Jana <12718297+B0Ja@users.noreply.github.com>
2020-06-10 23:38:05 +0200

    Add files via upload
    A version v.0.1 of the RBI data file which will help the basic function download requisite data.

---
+++ rbidata.py
@@ -0,0 +1,46 @@
+
+social_demographic = {
+    
+'total_population' : 
+['Total Population', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'rural_population' :
+['Population in Rural Area', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'urban_population':
+['Population in Urban Area', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'pop_growth_rate' :
+['Decadal Growth Rate of Population', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'sex_ratio':
+['Sex Ratio', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'literacy_rate':
+['Literacy Rate', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'density_pop' :
+['Density of Population', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'birth_rate' :
+['Birth Rate', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'death_rate': 
+['Death Rate', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'infant_mortality':
+['Infant Mortality Rate', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'life_expectancy':
+['Life Expectancy', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'unemp_rural' :
+['Unemployment Rate - Usual Status - Adjusted - Rural', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'unemp_urban' :
+['Unemployment Rate - Usual Status - Adjusted - Urban', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797'],
+
+'poverty_rate' :
+['Poverty Rate', 'https://www.rbi.org.in/Scripts/PublicationsView.aspx?id=18797']
+    
+}
